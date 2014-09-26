c32cadddab04df1acf66ce4a3fb5f575bf8b0ad8
Prajwal Tuladhar <praj@infynyxx.com>
2014-09-26 11:48:57 -0400

    adding more

--- src/main.rs
+++ src/main.rs
@@ -9,4 +9,52 @@ fn main() {
 
     let c = Circle::new(0.0, 0.0, 2.0);
     println!("area == {}", c.area());
+
+
+    let nums = [1, 2];
+    let noms = ["Jon", "John", "Ram", "Shyam"];
+    let mut odds = nums.iter().map(|&x| x * 2 - 1);
+    for num in odds {
+        spawn(proc() {
+            println!("{} says hello from lightweight thread", noms[num]);
+        });
+    }
+
+    // closure
+    let closure_args = |arg: int| -> () {
+        println!("closure_args: {}", arg + 1);
+    };
+
+    closure_args(10i);
+    closure_args(99i);
+
+    let closure = |x: int, y: int| -> () {
+        println!("{} - {} - {}", c.area(), x, y);
+    };
+
+    closure(10, 11);
+
+    let (channel, port) = channel();
+    spawn(proc() {
+        let result = 5i;
+        channel.send(result);
+    });
+
+    let result = port.recv();
+    println!("Result from task {}", result);
+
+    let mut count = 0i;
+    while count < 10000i {
+        count = count + 1i;
+        spawn(proc() {
+            println!("Count {}", count);
+        });
+    }
+
+    let msg = Some("Howdy");
+    println!("{}", msg);
+    match msg {
+        Some(ref m) => println!("{}", *m),
+        None => ()
+    }
 }
