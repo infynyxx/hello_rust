5ca8427626e4e09dd8baac03cd4849498e72dadc
Prajwal Tuladhar <praj@infynyxx.com>
2020-05-23 14:24:56 -0400

    fix tests

--- tests/lib.rs
+++ tests/lib.rs
@@ -4,8 +4,8 @@ use hello_rust::Circle;
 
 #[test]
 fn math_checks_out() {
-    let result = add_three_times_four(5i);
-    assert_eq!(32i, result);
+    let result = add_three_times_four(5i32);
+    assert_eq!(32i32, result);
 }
 
 #[test]
